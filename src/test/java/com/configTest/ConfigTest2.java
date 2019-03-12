525239bd573faebea7493fdbfdd71480c9c6802a
PHPawar <pprashanth.qa@gmail.com>
2019-03-12 09:45:46 +0530

    configTest2 included

---
+++ src/test/java/com/configTest/ConfigTest2.java
@@ -0,0 +1,18 @@
+package com.configTest;
+
+import java.io.IOException;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class ConfigTest2 {
+	PropMethod method;
+	
+	@Test
+	public void test() throws IOException
+	{
+	System.out.println("Testing from jenkins");
+	}
+	
+
+}
